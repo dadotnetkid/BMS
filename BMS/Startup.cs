170e135fc7126e82a46a29ceecd9b069be8c1620
Mark Christopher Cacal <markchristopher.cacal@outlook.com>
2019-09-18 05:55:51 +0800

    Add project files.

---
+++ BMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BMS.Startup))]
+namespace BMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
